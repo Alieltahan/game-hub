faee1a5318990ffbdae0b9ac7cc8936ef795e02f
Ali Eltahan <alyaltahan@gmail.com>
2026-04-29 00:36:38 +0300

    remove un-used imports

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,6 +1,6 @@
-import {FaWindows, FaXbox, FaPlaystation, FaLinux, FaAndroid, FaOsi, FaApple} from 'react-icons/fa';
+import {FaWindows, FaXbox, FaPlaystation, FaLinux, FaAndroid, FaApple} from 'react-icons/fa';
 import {Platform} from "../hooks/useGames";
-import {HStack, Icon, Text} from '@chakra-ui/react';
+import {HStack, Icon} from '@chakra-ui/react';
 import {BsGlobe} from "react-icons/bs";
 import {SiNintendo} from "react-icons/si";
 import {MdPhoneIphone} from "react-icons/md";
