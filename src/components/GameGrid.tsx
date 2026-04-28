e6d796261ef70d7afd3d57f715affb19af30b942
Ali Eltahan <alyaltahan@gmail.com>
2026-04-28 23:01:35 +0300

    Implement loading skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import {SimpleGrid, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardComponents";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-    const {games, error} = useGames();
-
+    const {games, error, isLoading} = useGames();
+    const sekeletonArray = Array().fill(6);
     if (error) {
         return <Text colorScheme="red">error</Text>
     }
 
     return (
         <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} spacing={5} padding={10}>
-            {games.map(game => <GameCard key={game.id} game={game}/>)}
+            <GameCardContainer>
+                {isLoading && sekeletonArray.map(sk => <GameCardSkeleton key={sk} />)}
+                {games.map(game => <GameCard key={game.id} game={game}/>)}
+            </GameCardContainer>
         </SimpleGrid>
     );
 };
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -24,21 +24,26 @@ interface FetchGameResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+
     useEffect(() => {
         setError(null)
         const controller = new AbortController();
+        setIsLoading(true);
+
         apiClient.get<FetchGameResponse>("/games", {signal: controller.signal})
             .then(res => setGames(res.data.results))
             .catch(err => {
                 if (err instanceof CanceledError) return;
                 setError(err.message)
-            })
+            }).finally(() => setIsLoading(false))
 
         return () => controller.abort();
     }, [])
     return {
         games,
-        error
+        error,
+        isLoading,
     }
 };
 
