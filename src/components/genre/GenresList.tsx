82793618c43163829dfcc7090c9896df8a0a41c4
Alieltahan <alyaltahan@gmail.com>
2026-04-29 23:25:35 +0300

    Fetching genre list

--- src/components/genre/GenresList.tsx
+++ src/components/genre/GenresList.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
 import useGenres from "../../hooks/useGenres";
 import {getCroppedUrl} from "../../services/utils";
+import GameCardContainer from "../game/GameCardContainer";
+import GenreSkeleton from "./GenreSkeleton";
 
 const GenresList = () => {
     const { data: genres, isLoading } = useGenres();
-
+    const skeletonArray = new Array(15).fill(1)
     return (
         <List>
+            {isLoading && skeletonArray.map((_sk, index) =>
+                <GameCardContainer key={index}>
+                    <GenreSkeleton/>
+                </GameCardContainer>
+            )}
             {genres.map((genre) => (
                 <ListItem key={genre.id} paddingY="5px">
                 <HStack>
