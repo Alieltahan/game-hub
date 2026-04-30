c636b5ba5610de0cf5cfadae664ae404e4adf643
Alieltahan <alyaltahan@gmail.com>
2026-04-30 22:59:16 +0300

    Fetch & render available platform with new Platform hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,54 @@
-import './App.css'
-import {Grid, GridItem, Show} from "@chakra-ui/react";
+import './App.css';
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import NavBar from "./components/NavBar";
-import {GenresList} from "./components/genre";
-import {GameGrid} from "./components/game";
+import { GenresList } from "./components/genre";
+import { GameGrid } from "./components/game";
+import PlatformSelector from "./components/PlatformSelector";
 import { Genre } from "./hooks/useGenres";
 
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+	const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
-  const handleSelectedGenre = (genre: Genre) => {
-    setSelectedGenre(genre);
-  }
+	const handleSelectedGenre = (genre: Genre) => {
+		setSelectedGenre(genre);
+	};
 
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"`
-  }}
-  templateColumns={{
-    base: "1fr",
-    lg: "200px 1fr"
-  }}>
-    <GridItem area="nav">
-      <NavBar/>
-    </GridItem>
-    <Show above="lg">
-      <GridItem area="aside" paddingX="5px">
-        <GenresList onSelectedGenre={handleSelectedGenre} selectedGenre={selectedGenre} />
-      </GridItem>
-    </Show>
-    <GridItem area="main">
-      <GameGrid selectedGenre={selectedGenre}/>
-    </GridItem>
-  </Grid>
+	function renderNavBar() {
+		return (
+			<GridItem area="nav">
+				<NavBar/>
+			</GridItem>);
+	};
+
+	function renderGenreList() {
+		return <Show above="lg">
+			<GridItem area="aside" paddingX="5px">
+				<GenresList onSelectedGenre={handleSelectedGenre} selectedGenre={selectedGenre}/>
+			</GridItem>
+		</Show>;
+	}
+
+	function renderGameGrid() {
+		return <GridItem area="main">
+			<PlatformSelector/>
+			<GameGrid selectedGenre={selectedGenre}/>
+		</GridItem>;
+	}
+
+	return <Grid
+		templateAreas={{
+			base: `"nav" "main"`,
+			lg: `"nav nav" "aside main"`
+		}}
+		templateColumns={{
+			base: "1fr",
+			lg: "200px 1fr"
+		}}>
+		{renderNavBar()}
+		{renderGenreList()}
+		{renderGameGrid()}
+	</Grid>;
 }
 
-export default App
+export default App;
